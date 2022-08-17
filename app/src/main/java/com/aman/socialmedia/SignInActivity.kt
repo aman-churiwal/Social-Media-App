1eaa4410f211a4e5f8905b367fab81025459596a
Amankumar Churiwal <amanchuriwal22@gmail.com>
2022-08-17 11:30:30 +0530

    implemented google signIn

---
+++ app/src/main/java/com/aman/socialmedia/SignInActivity.kt
@@ -0,0 +1,108 @@
+package com.aman.socialmedia
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN: Int = 123
+    private val TAG = "SignInActivity Tag"
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val signInRequest = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            // Your server's client ID, not your Android client ID.
+            .requestIdToken("614139050290-3qqui8v3jemddi1mi07kltolbcup1e9u.apps.googleusercontent.com")
+            // Only show accounts previously used to sign in.
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, signInRequest)
+        auth = Firebase.auth
+
+        signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try{
+            val account =
+                completedTask.getResult(ApiException::class.java)!!
+            Log.d(TAG,"firebaseAuthWithGoogle:"+account.id)
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException) {
+            Log.w(TAG,"signInResult:failed code"+e.statusCode)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        signInButton.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+        GlobalScope.launch(Dispatchers.IO) {
+            val auth = auth.signInWithCredential(credential).await()
+            val firebaseUser = auth.user
+            withContext(Dispatchers.Main){
+                updateUI(firebaseUser)
+            }
+        }
+    }
+
+    private fun updateUI(firebaseUser: FirebaseUser?) {
+        if(firebaseUser!=null) {
+            val mainActivityIntent = Intent(this,MainActivity::class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        } else {
+            signInButton.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+        }
+    }
+}
